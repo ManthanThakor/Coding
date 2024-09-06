758aad79fade170f278695fe42c2b2e4b75c1f03
Manthan Thakor <thakormanthan849@gmail.com>
2024-09-06 09:28:40 +0530

    Codeing 2

--- Script.js
+++ Script.js
@@ -1,5 +1,30 @@
-const text = document.getElementsByClassName("id");
-text[0].innerText = "Hello, World!";
+// const text = document.getElementsByClassName("id");
+// text[0].innerText = "Hello, World!";
 
-console.log("hello");
-console.log("hello");
+// console.log("hello");
+
+// -------------------
+
+// let a, b, c;
+// a = 5;
+// b = 4;
+// c = a + b;
+// console.log(c);
+// -----------------------
+
+// check the number between range 1 to 5 and 5 to 7
+
+function checkNum(num) {
+  const range = [1, 5];
+  const range2 = [5, 7];
+  if (
+    (num > range[0] && num < range[1]) ||
+    (num > range2[0] && num < range2[1])
+  )
+    return num;
+  else {
+    return null;
+  }
+}
+
+console.log(checkNum(2));
