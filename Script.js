1822c9aac38f291b6c10c6c1056e8c27cb6d7c1f
Manthan Thakor <thakormanthan849@gmail.com>
2024-09-07 16:32:44 +0530

    Codeing 4

--- Script.js
+++ Script.js
@@ -12,7 +12,7 @@
 // console.log(c);
 // -----------------------
 
-// check the number between range 1 to 5 and 5 to 7
+//! check the number between range 1 to 5 and 5 to 7
 
 function checkNum(num) {
   const range = [1, 5];
@@ -21,10 +21,44 @@ function checkNum(num) {
     (num > range[0] && num < range[1]) ||
     (num > range2[0] && num < range2[1])
   )
-    return num;
+    return true;
   else {
     return null;
   }
 }
 
-console.log(checkNum(2));
+console.log(checkNum(9));
+
+//! Get the value between two number
+
+function getValue(start, end) {
+  const ans = [];
+  for (let i = start + 1; i < end; i++) {
+    ans.push(i);
+  }
+  return ans;
+}
+console.log(getValue(2, 5));
+
+//! --------------------------
+
+const a = [1, 2, 3];
+const b = [2, 3, 5];
+console.log(a + b);
+
+// REVERSE
+
+let stt = " ulta karo hame ";
+function rev(st) {
+  return st.trim().split("").reverse().join("");
+}
+console.log(`Reverse String:`, rev(stt));
+
+let names = ["John", "Anna", "bob", "jane"];
+names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+// --------------------
+
+let arr = [1, 2, 3];
+let allLessThanFive = arr.every((x) => x < 2);
+console.log(allLessThanFive); // Output: true
