7e385216629dc5f15750e2f2c7256e8a78dc9ca5
Manthan Thakor <thakormanthan849@gmail.com>
2024-09-28 16:13:17 +0530

    completed 2

--- Script.js
+++ Script.js
@@ -1296,6 +1296,27 @@ function printLeftAngleTriangle(n) {
 // *****
 
 //! ----------------------------------------------
+//? ---- printReversedLeftAngleTriangle ---
+//! ---------------------------------------------
+
+function printReversedLeftAngleTriangle(n) {
+  for (let i = n; i >= 1; i--) {
+    let stars = "";
+    for (let j = 1; j <= i; j++) {
+      stars += "*"; // Add stars
+    }
+    console.log(stars); // Print each row
+  }
+}
+
+// printReversedLeftAngleTriangle(5);
+// *****
+// ****
+// ***
+// **
+// *
+
+//! ----------------------------------------------
 //? ---- square ---
 //! ---------------------------------------------
 
